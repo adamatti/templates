c18b90579e83ce2b863771811a7348ad158dd6fb
Marcelo Adamatti <adamatti@gmail.com>
2025-09-23 11:57:51 -0300

    chore: lint

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,16 +1,17 @@
-import path from 'node:path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
+    // biome-ignore lint: check it later
     port: process.env.PORT ?? 3000,
-  }
-})
+  },
+});
