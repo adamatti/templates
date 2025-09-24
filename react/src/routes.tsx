cf6253584330b791149b9b3459d408e5f8cdb59f
Marcelo Adamatti <adamatti@gmail.com>
2025-09-24 01:18:28 -0300

    chore: lint

--- react/src/routes.tsx
+++ react/src/routes.tsx
@@ -1,15 +1,15 @@
-import { Route, BrowserRouter, Routes as ReactRoutes } from "react-router-dom";
-import MainPage from "./pages/main.page";
+import { BrowserRouter, Routes as ReactRoutes, Route } from "react-router-dom";
 import AboutPage from "./pages/about.page";
-import "./app.css"
+import MainPage from "./pages/main.page";
+import "./app.css";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-            <ReactRoutes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/about" element={<AboutPage />} />
-            </ReactRoutes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <ReactRoutes>
+        <Route element={<MainPage />} path="/" />
+        <Route element={<AboutPage />} path="/about" />
+      </ReactRoutes>
+    </BrowserRouter>
+  );
 }
