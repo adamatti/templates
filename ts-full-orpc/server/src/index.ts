1b9da7ee21de4717e382055eea76cb8cf4ca151a
Marcelo Adamatti <adamatti@gmail.com>
2025-09-25 12:01:49 -0300

    fix: lint

--- ts-full-orpc/server/src/index.ts
+++ ts-full-orpc/server/src/index.ts
@@ -1,4 +1,4 @@
 // Add exports to frontend here
 
-export type { Todo } from './service/types';
 export type { default as router } from './orpc/router';
+export type { Todo } from './service/types';
